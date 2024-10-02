be90d3bc10f97bcc2b36bf6f4b88b6be2d065341
Miguel Sanabria <majsb.sanabria@gmail.com>
2024-10-02 14:59:03 -0400

    proyecto POO objeto

---
+++ Poo/objeto.py
@@ -0,0 +1,7 @@
+class perro:
+    def __init__(self, nombre, edad, raza):
+        # Dentro del código del constructor vamos a definir el estado
+        # inicial de cualquier perro que sea creado
+        self.name = nombre
+        self.age = edad
+        self.race = raza
